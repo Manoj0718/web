d362ea8149a7ddf348f047c8f62a8bf7147bc39b
Manoj0718 <flyindessert@gmail.com>
2023-08-16 21:25:40 +0200

    node error fixed

---
+++ .nuxt/dist/server/components/text-side.js
@@ -0,0 +1,144 @@
+exports.ids = [7];
+exports.modules = {
+
+/***/ 115:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_text_Side_vue_vue_type_style_index_0_id_7b242a19_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(99);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_text_Side_vue_vue_type_style_index_0_id_7b242a19_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_text_Side_vue_vue_type_style_index_0_id_7b242a19_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_text_Side_vue_vue_type_style_index_0_id_7b242a19_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_text_Side_vue_vue_type_style_index_0_id_7b242a19_prod_lang_scss___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 116:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(3);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(function(i){return i[1]});
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, "@keyframes rotation{0%{transform:rotate(0deg)}to{transform:rotate(359deg)}}@keyframes glitch{0%{-webkit-clip-path:var(--clip-one);clip-path:var(--clip-one)}2%,8%{-webkit-clip-path:var(--clip-two);clip-path:var(--clip-two);transform:translate(calc(var(--shimmy-distance)*-1%))}6%{-webkit-clip-path:var(--clip-two);clip-path:var(--clip-two);transform:translate(calc(var(--shimmy-distance)*1%))}9%{-webkit-clip-path:var(--clip-two);clip-path:var(--clip-two);transform:translate(0)}10%{-webkit-clip-path:var(--clip-three);clip-path:var(--clip-three);transform:translate(calc(var(--shimmy-distance)*1%))}13%{-webkit-clip-path:var(--clip-three);clip-path:var(--clip-three);transform:translate(0)}14%,21%{-webkit-clip-path:var(--clip-four);clip-path:var(--clip-four);transform:translate(calc(var(--shimmy-distance)*1%))}25%{-webkit-clip-path:var(--clip-five);clip-path:var(--clip-five);transform:translate(calc(var(--shimmy-distance)*1%))}30%{-webkit-clip-path:var(--clip-five);clip-path:var(--clip-five);transform:translate(calc(var(--shimmy-distance)*-1%))}35%,45%{-webkit-clip-path:var(--clip-six);clip-path:var(--clip-six);transform:translate(calc(var(--shimmy-distance)*-1%))}40%{-webkit-clip-path:var(--clip-six);clip-path:var(--clip-six);transform:translate(calc(var(--shimmy-distance)*1%))}50%{-webkit-clip-path:var(--clip-six);clip-path:var(--clip-six);transform:translate(0)}55%{-webkit-clip-path:var(--clip-seven);clip-path:var(--clip-seven);transform:translate(calc(var(--shimmy-distance)*1%))}60%{-webkit-clip-path:var(--clip-seven);clip-path:var(--clip-seven);transform:translate(0)}31%,61%,to{-webkit-clip-path:var(--clip-four);clip-path:var(--clip-four)}}@keyframes anime{1%{transform:rotateX(10deg) skewX(90deg)}2%{transform:rotateY(10deg) skewY(90deg)}}.main_container{align-items:center;color:gold;display:flex;flex-direction:column;justify-content:center;margin:0 auto;width:100%}@media screen and (max-width:768px){.main_container{height:100%;margin:0 auto}}.main_container .hero_text{align-items:center;color:gold;display:flex;flex-direction:column;font-size:1.2rem;height:90vh;justify-content:center;margin:10vh auto;width:90%}@media screen and (max-width:768px){.main_container .hero_text{display:flex;flex-direction:column;justify-content:center;margin:10vh 0 0;padding:0;width:100%}}.main_container .hero_text .fixed_text{color:gold;padding:20px}@media screen and (max-width:768px){.main_container .hero_text .fixed_text{align-items:center;font-size:18px;padding:3px}}.main_container .hero_text #animationText{color:gold;margin:5vh;padding:20px;text-align:center}@media screen and (max-width:768px){.main_container .hero_text #animationText{font-size:18px;height:30vh;margin:0;padding:2px}}.main_container .hero_text #animationText .fixed{margin:5vh;padding:10px}@media screen and (max-width:768px){.main_container .hero_text #animationText .fixed{margin:0;padding:2px}}.main_container .hero_text #animationText .typing{font-style:oblique;margin-top:20px;padding:10px}@media screen and (max-width:768px){.main_container .hero_text #animationText .typing{margin:0;padding:2px}}.main_container .hero_text .conatct_button{border:2px solid rgba(85,191,64,.311);box-shadow:1px 0 10px rgba(43,255,0,.907);margin:10px}@media screen and (max-width:768px){.main_container .hero_text .conatct_button{margin-top:.5vh}}.main_container .hero_text .conatct_button .cybr-btn{--primary:hsl(var(--primary-hue),85%,calc(var(--primary-lightness, 50)*1%));--shadow-primary:hsl(var(--shadow-primary-hue) 90% 50%);--primary-hue:120;--primary-lightness:50;--color:#fff;--font-size:1rem;--shadow-primary-hue:180;--label-size:9px;--shadow-secondary-hue:60;--shadow-secondary:hsl(var(--shadow-secondary-hue) 90% 60%);--clip:polygon(0 0,100% 0,100% 100%,95% 100%,95% 90%,85% 90%,85% 100%,8% 100%,0 70%);--border:2px;--shimmy-distance:5;--clip-one:polygon(0 2%,100% 2%,100% 95%,95% 95%,95% 90%,85% 90%,85% 95%,8% 95%,0 70%);--clip-two:polygon(0 78%,100% 78%,100% 100%,95% 100%,95% 90%,85% 90%,85% 100%,8% 100%,0 78%);--clip-three:polygon(0 44%,100% 44%,100% 54%,95% 54%,95% 54%,85% 54%,85% 54%,8% 54%,0 54%);--clip-four:polygon(0 0,100% 0,100% 0,95% 0,95% 0,85% 0,85% 0,8% 0,0 0);--clip-five:polygon(0 0,100% 0,100% 0,95% 0,95% 0,85% 0,85% 0,8% 0,0 0);--clip-six:polygon(0 40%,100% 40%,100% 85%,95% 85%,95% 85%,85% 85%,85% 85%,8% 85%,0 70%);--clip-seven:polygon(0 63%,100% 63%,100% 80%,95% 80%,95% 80%,85% 80%,85% 80%,8% 80%,0 70%);background:transparent;border:0;color:#fff;color:var(--color);cursor:pointer;font-family:\"Cyber\",sans-serif;font-size:1rem;font-size:var(--font-size);font-weight:200;height:50px;letter-spacing:2px;line-height:2.5;min-width:200px;outline:transparent;position:relative;text-transform:uppercase;transition:background .2s}.main_container .hero_text .conatct_button .cybr-btn:hover{background:rgba(64,191,106,.516);box-shadow:0 0 25px rgba(43,255,0,.907)}.main_container .hero_text .conatct_button .cybr-btn:hover .cybr-btn__glitch{display:block}.main_container .hero_text .social_button{margin-top:1vh}.main_container .hero_text .slogen{color:#f5eff5;font-size:1rem;font-style:italic;padding:1vh}@media screen and (max-width:768px){.main_container .hero_text .slogen{font-size:.8rem;margin:1vh 0;text-align:center}}.main_container .skill{color:#f5eff5;height:90vh;margin:auto;padding:5vh}@media screen and (max-width:768px){.main_container .skill{height:100%;margin:0 1vh 1vh;padding:0}}", ""]);
+// Exports
+___CSS_LOADER_EXPORT___.locals = {};
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 126:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/text-Side.vue?vue&type=template&id=7b242a19&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "main_container"
+  }, [_vm._ssrNode("<div class=\"hero_text\">", "</div>", [_vm._ssrNode("<div id=\"animationText\">", "</div>", [_c('vue-typed-js', {
+    attrs: {
+      "strings": _vm.msgs,
+      "typeSpeed": 120,
+      "loop": true,
+      "startDelay": 2000,
+      "cursorChar": '_'
+    }
+  }, [_c('h1', [_c('span', {
+    staticClass: "fixed"
+  }, [_vm._v("Hi, I'm Manoj.")]), _vm._v(" "), _c('br'), _vm._v(" "), _c('span', {
+    staticClass: "typing"
+  })])])], 1), _vm._ssrNode(" <div class=\"conatct_button\"><button class=\"cybr-btn\">View My Cv</button></div> "), _vm._ssrNode("<div class=\"social_button\">", "</div>", [_c('SocialMediaContact')], 1), _vm._ssrNode(" <p class=\"slogen\"><span> 🔥 </span>My motto: face challenges with a smile and never stop growing.<span> 🔥 </span></p>")], 2), _vm._ssrNode(" "), _vm._ssrNode("<div class=\"skill\">", "</div>", [_c('LazyMySkillComponenet')], 1)], 2);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/text-Side.vue?vue&type=template&id=7b242a19&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/text-Side.vue?vue&type=script&lang=js&
+/* harmony default export */ var text_Sidevue_type_script_lang_js_ = ({
+  name: 'hero_text',
+  mounted() {
+    this.setAnimation();
+  },
+  data() {
+    return {
+      msgs: ['Javascript Developer!', 'Frontend Developer!']
+    };
+  },
+  methods: {
+    setAnimation() {
+      this.$anime({
+        targets: '.fixed_text',
+        //translateX: 90,
+        // rotate: '2turn',
+        duration: 800
+      });
+    },
+    openFile() {
+      console.log('clcik');
+      window.open('https://drive.google.com/file/d/1qrN1QZTdy2J4jDoFogfFxeIuB3NnvSow/view?usp=sharing');
+    }
+  }
+});
+// CONCATENATED MODULE: ./components/text-Side.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_text_Sidevue_type_script_lang_js_ = (text_Sidevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/text-Side.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(115)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_text_Sidevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "68f5a98c"
+  
+)
+
+/* harmony default export */ var text_Side = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {SocialMediaContact: __webpack_require__(38).default})
+
+
+/***/ }),
+
+/***/ 99:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(116);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(4).default
+module.exports.__inject__ = function (context) {
+  add("4e4fe9dc", content, true, context)
+};
+
+/***/ })
+
+};;
+//# sourceMappingURL=text-side.js.map
